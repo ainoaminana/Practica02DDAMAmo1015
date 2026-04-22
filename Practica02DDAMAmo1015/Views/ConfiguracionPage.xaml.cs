6ef07f164625c74bf29102fa15e297fb14c90a4b
Ainoa Miñana Ortolà <amo1015@alu.ubu.es>
2026-04-22 03:49:49 +0200

    Mejorado el aspecto visual de la página de configuración y añadidas las opcioines de personalización.

--- Practica02DDAMAmo1015/Views/ConfiguracionPage.xaml.cs
+++ Practica02DDAMAmo1015/Views/ConfiguracionPage.xaml.cs
@@ -1,9 +1,41 @@
-namespace Practica02DDAMAmo1015.Views;
+﻿namespace Practica02DDAMAmo1015.Views;
 
 public partial class ConfiguracionPage : ContentPage
 {
-	public ConfiguracionPage()
-	{
-		InitializeComponent();
-	}
+    public ConfiguracionPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnTextSizeChanged(object sender, ValueChangedEventArgs e)
+    {
+        Application.Current.Resources["GlobalFontSize"] = e.NewValue;
+    }
+
+    private void OnColorChanged(object sender, EventArgs e)
+    {
+        string color = ColorPicker.SelectedItem as string;
+
+        Application.Current.Resources["GlobalFontColor"] = color switch
+        {
+            "Negro" => Colors.Black,
+            "Rojo" => Colors.Red,
+            "Azul" => Colors.Blue,
+            "Verde" => Colors.Green,
+            _ => Colors.Black
+        };
+    }
+
+    private void OnFontChanged(object sender, EventArgs e)
+    {
+        string font = FontPicker.SelectedItem as string;
+
+        Application.Current.Resources["GlobalFontFamily"] = font switch
+        {
+            "Sans" => "sans-serif",
+            "Serif" => "serif",
+            "Monospace" => "monospace",
+            _ => "sans-serif"
+        };
+    }
 }
