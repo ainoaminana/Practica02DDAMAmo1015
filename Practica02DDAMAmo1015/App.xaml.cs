fc051735fa37f2f8da5a82ee333bd29730d19e21
Ainoa Miñana Ortolà <amo1015@alu.ubu.es>
2026-04-22 03:26:36 +0200

    Añadidos cambios para mejorar el aspecto visual de las páginas de Login y Home, además de corregir algunas líneas para asegurar la correcta navegación por la página a través del Shell.

--- Practica02DDAMAmo1015/App.xaml.cs
+++ Practica02DDAMAmo1015/App.xaml.cs
@@ -1,12 +1,11 @@
-﻿namespace Practica02DDAMAmo1015
-{
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
+﻿namespace Practica02DDAMAmo1015;
 
-            MainPage = new AppShell();
-        }
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new NavigationPage(new Views.LoginPage());
     }
 }
--- Practica02DDAMAmo1015/Views/HomePage.xaml.cs
+++ Practica02DDAMAmo1015/Views/HomePage.xaml.cs
@@ -2,8 +2,18 @@ namespace Practica02DDAMAmo1015.Views;
 
 public partial class HomePage : ContentPage
 {
-	public HomePage()
-	{
-		InitializeComponent();
-	}
+    public HomePage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnGaleriaClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//galeria");
+    }
+
+    private async void OnConfigClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//configuracion");
+    }
 }
